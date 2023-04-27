da46ca1e5ee8e025e6d6d2647752e9e8e1e85c35
XuehaoSun <xuehao.sun@intel.com>
2023-04-27 14:45:24 +0800

    for test
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- .github/workflows/script/install_binary.sh
+++ .github/workflows/script/install_binary.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 source /intel-extension-for-transformers/.github/workflows/script/change_color.sh
 
-cd /intel-extension-for-transformers
-$BOLD_YELLOW && echo "---------------- git submodule update --init --recursive -------------" && $RESET
-git config --global --add safe.directory /intel-extension-for-transformers
-git config --global --add safe.directory /intel-extension-for-transformers/intel_extension_for_transformers/backends/neural_engine/third_party/boost/libs/assert
-git submodule update --init --recursive
+pip install intel-extension-for-transformers
+# cd /intel-extension-for-transformers
+# $BOLD_YELLOW && echo "---------------- git submodule update --init --recursive -------------" && $RESET
+# git config --global --add safe.directory /intel-extension-for-transformers
+# git config --global --add safe.directory /intel-extension-for-transformers/intel_extension_for_transformers/backends/neural_engine/third_party/boost/libs/assert
+# git submodule update --init --recursive
 
-$BOLD_YELLOW && echo "---------------- run python setup.py sdist bdist_wheel -------------" && $RESET
-python setup.py sdist bdist_wheel
+# $BOLD_YELLOW && echo "---------------- run python setup.py sdist bdist_wheel -------------" && $RESET
+# python setup.py sdist bdist_wheel
 
-$BOLD_YELLOW && echo "---------------- pip install binary -------------" && $RESET
-pip install dist/intel_extension_for_transformers*.whl
-pip list
+# $BOLD_YELLOW && echo "---------------- pip install binary -------------" && $RESET
+# pip install dist/intel_extension_for_transformers*.whl
+# pip list
