bd9c18eb1657acfd4598953f46b8d9f089ee83c8
XuehaoSun <xuehao.sun@intel.com>
2023-04-27 14:31:12 +0800

    for test
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- .github/workflows/script/install_binary.sh
+++ .github/workflows/script/install_binary.sh
@@ -4,6 +4,7 @@ source /intel-extension-for-transformers/.github/workflows/script/change_color.s
 cd /intel-extension-for-transformers
 $BOLD_YELLOW && echo "---------------- git submodule update --init --recursive -------------" && $RESET
 git config --global --add safe.directory /intel-extension-for-transformers
+git config --global --add safe.directory /intel-extension-for-transformers/intel_extension_for_transformers/backends/neural_engine/third_party/boost/libs/assert
 git submodule update --init --recursive
 
 $BOLD_YELLOW && echo "---------------- run python setup.py sdist bdist_wheel -------------" && $RESET
