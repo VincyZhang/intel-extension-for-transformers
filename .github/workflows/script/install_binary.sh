3ce0e426cdb0a7210a59236fd81bd68863ab0238
XuehaoSun <xuehao.sun@intel.com>
2023-04-27 14:06:25 +0800

    for test
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- .github/workflows/script/install_binary.sh
+++ .github/workflows/script/install_binary.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+source /intel-extension-for-transformers/.github/workflows/script/change_color.sh
 
 cd /intel-extension-for-transformers
-python -m pip install --no-cache-dir -r requirements.txt
+
+$BOLD_YELLOW && echo "---------------- run python setup.py sdist bdist_wheel -------------" && $RESET
 python setup.py sdist bdist_wheel
+
+$BOLD_YELLOW && echo "---------------- pip install binary -------------" && $RESET
 pip install dist/intel_extension_for_transformers*.whl
 pip list
--- .github/workflows/script/unitTest/run_unit_test_optimize.sh
+++ .github/workflows/script/unitTest/run_unit_test_optimize.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source /intel-extension-for-transformers/.github/workflows/script/change_color.sh
+
 cd /intel-extension-for-transformers/tests || exit 1
 find ./ -name "test*.py" | sed 's,\.\/,python ,g' | sed 's/$/ --verbose/' >run.sh
 
@@ -7,14 +9,14 @@ JOB_NAME=unit_test
 mkdir -p ${LOG_DIR}
 ut_log_name=${LOG_DIR}/ut_${JOB_NAME}.log
 
-echo "cat run.sh..."
+$BOLD_YELLOW && echo "cat run.sh..." && $RESET
 cat run.sh | tee ${ut_log_name}
-echo "------UT start-------"
+$BOLD_YELLOW && echo "------UT start-------" && $RESET
 bash run.sh 2>&1 | tee -a ${ut_log_name}
-echo "------UT end -------"
+$BOLD_YELLOW && echo "------UT end -------" && $RESET
 
 if [ $(grep -c "FAILED" ${ut_log_name}) != 0 ] || [ $(grep -c "OK" ${ut_log_name}) == 0 ]; then
-    echo "Find errors in UT test, please check the output..."
+    $BOLD_RED && echo "Find errors in UT test, please check the output..." && $RESET
     exit 1
 fi
-echo "UT finished successfully! "
+$BOLD_GREEN && echo "UT finished successfully! " && $RESET
