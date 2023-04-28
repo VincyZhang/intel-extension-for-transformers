047c1e97299236d008959c9d1fe6db04d488ca29
XuehaoSun <xuehao.sun@intel.com>
2023-04-28 15:54:39 +0800

    for test
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- .github/workflows/script/formatScan/clangformat.sh
+++ .github/workflows/script/formatScan/clangformat.sh
@@ -25,7 +25,7 @@ cd /intel-extension-for-transformers
 git config --global --add safe.directory "*"
 
 echo $target_branch
-target_branch=$(echo $(git show-ref -s remotes/origin/$target_branch))
+target_branch="$(git show-ref -s "remotes/origin/$target_branch")"
 echo $target_branch
 echo "git diff $target_branch /intel-extension-for-transformers"
 git diff $target_branch /intel-extension-for-transformers 2>&1 | tee -a ${log_path}
--- .github/workflows/script/formatScan/test.sh
+++
@@ -1,3 +0,0 @@
-target_branch=main
-target_branch=$(echo $(git show-ref -s remotes/origin/${target_branch}))
-echo $target_branch
