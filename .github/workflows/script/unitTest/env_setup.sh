747c5a4b7ef6917d4c2f28bb6ddb96436873b9b9
Wenxin Zhang <wenxin.zhang@intel.com>
2023-05-24 16:38:00 +0800

    try upload/download

--- .github/workflows/script/unitTest/env_setup.sh
+++ .github/workflows/script/unitTest/env_setup.sh
@@ -8,5 +8,15 @@ until [ "$n" -ge 5 ]; do
     sleep 5
 done
 
+# Install test requirements
+cd ./tests
+if [ -f "requirements.txt" ]; then
+    sed -i '/neural-compressor/d;/tensorflow==/d;/torch==/d;/pytorch-ignite$/d;/mxnet==/d;/mxnet-mkl==/d;/torchvision==/d;/onnx$/d;/onnx==/d;/onnxruntime$/d;/onnxruntime==/d' requirements.txt
+    python -m pip install --default-timeout=100 -r requirements.txt
+    pip list
+else
+    echo "Not found requirements.txt file."
+fi
+
 pip install coverage
 pip install pytest
