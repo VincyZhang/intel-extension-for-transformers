15226260b3da181d9f14bc9cd459140118d1abf8
Wenxin Zhang <wenxin.zhang@intel.com>
2023-05-24 16:38:00 +0800

    try upload/download

--- .github/workflows/script/prepare_env.sh
+++ .github/workflows/script/prepare_env.sh
@@ -8,13 +8,3 @@ if [ -f "requirements.txt" ]; then
 else
     echo "Not found requirements.txt file."
 fi
-
-# Install test requirements
-cd ./tests
-if [ -f "requirements.txt" ]; then
-    sed -i '/neural-compressor/d;/tensorflow==/d;/torch==/d;/pytorch-ignite$/d;/mxnet==/d;/mxnet-mkl==/d;/torchvision==/d;/onnx$/d;/onnx==/d;/onnxruntime$/d;/onnxruntime==/d' requirements.txt
-    python -m pip install --default-timeout=100 -r requirements.txt
-    pip list
-else
-    echo "Not found requirements.txt file."
-fi
