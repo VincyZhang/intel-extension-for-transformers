6a799cd051aeed1f384ecb8d51381ac9643d771a
Wenxin Zhang <wenxin.zhang@intel.com>
2024-03-11 11:24:36 +0800

    update with error output
    Signed-off-by: Wenxin Zhang <wenxin.zhang@intel.com>

--- .github/workflows/script/ask_neuralchat.py
+++ .github/workflows/script/ask_neuralchat.py
@@ -71,9 +71,10 @@ def get_issues_comment():
                 print("This Comment is From Developer %s : %s END" % (owner, body))
                 messages.append({"role": "assistant", "content": body })
             print("Final Messages is: %s " % str(messages))
+        return messages
     except:
         logging.error("Get Issues Comment Failed")
-    return messages
+    
 
 def filter_comment(user_content: str):
     comment_list = ["If you need help, please @NeuralChat",
@@ -102,9 +103,9 @@ def request_neuralchat_bot(user_content: str):
             logging.error("Get Empty NeuralChatBot Response")
         else:
             print("Get NeuralChatBot Response: %s" % output)
+            return output
     except:
         logging.error("Request NeuralChatBot Failed")
-    return output
 
 
 def request_neuralchat_bot_with_history(messages: list):
@@ -126,9 +127,9 @@ def request_neuralchat_bot_with_history(messages: list):
             logging.error("Get Empty NeuralChatBot Response")
         else:
             print("Get NeuralChatBot Response with Context History: %s" % output)
+        return output
     except:
         logging.error("Request NeuralChatBot with Context History Failed")
-    return output
 
 def update_comment(resp: str):
     url = 'https://api.github.com/repos/VincyZhang/intel-extension-for-transformers/issues/%s/comments' % issue_number
